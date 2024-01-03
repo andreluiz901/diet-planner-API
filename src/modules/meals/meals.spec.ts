092207d0f738159aeb2c5a0e6e62d97a3c5367cb
André Luiz <silva.ala82@gmail.com>
2024-01-02 22:28:17 -0300

    test: create test for update meal route

--- src/modules/meals/meals.spec.ts
+++ src/modules/meals/meals.spec.ts
@@ -72,4 +72,64 @@ describe("Meals Routes", () => {
     expect(newMeal.body.data[0].id).toBeTypeOf("string");
     expect(newMeal.body.data[0].user_id).toBeTypeOf("string");
   });
+
+  it("should be able to a user update  meal", async () => {
+    const newUser = await request(app.server)
+      .post("/users")
+      .send({
+        name: "name of user",
+        username: "valid_",
+        email: "valid@email.com",
+        password: "validPassword@123",
+        confirm_password: "validPassword@123",
+        age: 22,
+        current_weight: 100,
+        goal_weight: 80,
+        goal: "LOSE",
+      })
+      .expect(201);
+
+    const loggedInUser = await request(app.server)
+      .post("/auth/signIn")
+      .send({
+        email: "valid@email.com",
+        password: "validPassword@123",
+      })
+      .expect(200);
+
+    const newMeal = await request(app.server)
+      .post("/meals")
+      .set("Authorization", `Bearer ${loggedInUser.body.access_token}`)
+      .send({
+        name: "refeição de hoje",
+        description: "batata doce com frango",
+        date_hour: "2024-01-02T03:24:00",
+        is_on_diet: true,
+      })
+      .expect(201);
+
+    const updatedMeal = await request(app.server)
+      .put(`/meals/${newMeal.body.data[0].id}`)
+      .set("Authorization", `Bearer ${loggedInUser.body.access_token}`)
+      .send({
+        name: "refeição updated",
+        description: "modified",
+        date_hour: "2024-01-01T08:24:00",
+        is_on_diet: false,
+      });
+
+    expect(updatedMeal.body).toEqual(
+      expect.objectContaining({
+        id: newMeal.body.data[0].id,
+        name: "refeição updated",
+        description: "modified",
+        date_hour: new Date("2024-01-01T08:24:00").toISOString(),
+        is_on_diet: 0,
+        user_id: newUser.body.data.id,
+      })
+    );
+
+    // expect(updatedMeal.body.id).toBeTypeOf("string");
+    // expect(updatedMeal.body.user_id).toBeTypeOf("string");
+  });
 });

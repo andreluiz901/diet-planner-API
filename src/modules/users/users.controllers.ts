0232efea2ffff2aa151d37ec5ae2b79207a8b06b
André Luiz <silva.ala82@gmail.com>
2024-01-03 00:06:15 -0300

    feat: create user summary route

--- src/modules/users/users.controllers.ts
+++ src/modules/users/users.controllers.ts
@@ -4,6 +4,7 @@ import { randomUUID } from "crypto";
 import { z } from "zod";
 import { findOneByEmail, findOneByUsername } from "./users.services";
 import { encryptString } from "../../utils/encrypt-utils";
+import { authorization } from "../auth/signIn.auth.service";
 
 export async function userRoutes(app: FastifyInstance) {
   app.post("/", async (req, reply) => {
@@ -90,5 +91,44 @@ export async function userRoutes(app: FastifyInstance) {
     }
 
     return reply.status(404).send();
+  });
+
+  app.get("/summary", { preHandler: authorization }, async (req, reply) => {
+    const userId = req.id;
+
+    const userMeals = await dbKnex("meals").select().where({ user_id: userId });
+
+    const meals_on_diet = await dbKnex("meals")
+      .select()
+      .where({ user_id: userId })
+      .andWhere({ is_on_diet: true })
+      .count();
+    const meals_off_diet = await dbKnex("meals")
+      .select()
+      .where({ user_id: userId })
+      .andWhere({ is_on_diet: false })
+      .count();
+
+    const streaks: any = await dbKnex("meals")
+      .select("date_hour", "is_on_diet")
+      .where({ user_id: userId })
+      .groupBy("date_hour")
+      .count("* as count");
+
+    let streak = 0;
+    let best_streak = 0;
+    for (let item of streaks) {
+      console.log(item);
+      if (streak > best_streak) best_streak = streak;
+      if (item.is_on_diet) streak += item.count;
+      if (!item.is_on_diet) streak = 0;
+    }
+
+    return reply.status(200).send({
+      meals_count: userMeals.length,
+      meals_on_diet: meals_on_diet[0]["count(*)"],
+      meals_off_diet: meals_off_diet[0]["count(*)"],
+      best_streak,
+    });
   });
 }
